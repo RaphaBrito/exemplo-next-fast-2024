f097416939c9ab904ed75202d86806c0b6182b7b
Raphael Brito <raphael.brito@hp.com>
2024-06-28 21:07:39 -0300

    Introdução - pág. contato

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactNumber from "@/components/ContactNumber";
+
+export default function Contact() {
+  return (
+    <div className="p-10">
+      <h1>Contato que acabamos de criar</h1>
+      <ContactNumber />
+    </div>
+  );
+}
