f73a15df4abf10122cfca7e97271d84829ae4627
Raphael Brito <raphael.brito@hp.com>
2024-06-28 22:53:24 -0300

    Exemplos de client e server side rendering

--- src/app/blog/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlogPost({ params }: { params: { id: number } }) {
-  return (
-    <div className="p-20">
-      <h1>Blog Post {params.id}</h1>
-      <p>Este é o conteúdo do Post {params.id}.</p>
-    </div>
-  );
-}
